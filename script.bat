d145d0da37cac54ada0cf17fb06debc7b6a5d3a0
9PA$A <104719904+IAM9PASA@users.noreply.github.com>
2025-11-08 00:28:18 +0700

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM -- Roblox place info
+set "UNIVERSE_ID=6055893858"
+set "PLACE_ID=94541876566503"
+
+REM -- Path to Roblox versions
+set "ROBLOX_VERSIONS=%localappdata%\Roblox\Versions"
+
+REM -- Find the most recent version folder with Roblox Studio
+set "STUDIO_EXE="
+for /f "delims=" %%i in ('dir "%ROBLOX_VERSIONS%\version-*" /ad /b /o-d') do (
+    set "VERSION_PATH=%ROBLOX_VERSIONS%\%%i"
+    for %%f in ("!VERSION_PATH!\*Studio*.exe") do (
+        if exist "%%~f" (
+            set "STUDIO_EXE=%%~f"
+            set "LATEST_VERSION=%%i"
+            goto launch
+        )
+    )
+)
+
+echo ❌ No usable Roblox Studio executable found in any version folder.
+pause
+exit /b
+
+:launch
+echo ✅ Found Studio executable: !STUDIO_EXE!
+echo ✅ In version: !LATEST_VERSION!
+start "" "!STUDIO_EXE!" -startEvent www.roblox.com/robloxQTStudioStartedEvent -task EditPlace -universeId %UNIVERSE_ID% -placeId %PLACE_ID%
